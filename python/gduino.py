13f47646fbebab670b44689fcaccffe6d16a55a2
mattpie <mpiechowski@gmail.com>
2015-05-16 10:34:12 -0600

    updating file

--- python/gduino.py
+++ python/gduino.py
@@ -1,4 +1,7 @@
-# example
+'''
+check gmail and light an LED
+if there are unread emails
+'''
 
 from mail import Mail
 from sConnection import SConnection
@@ -35,37 +38,3 @@ def main():
 if __name__ == '__main__': main()
 
 
-"""
-Example to check gmail and light an LED
-if there are unread emails
-
-from mail import Mail
-from sConnection import SConnection
-import sys
-
-MAIL    = 'M'
-NO_MAIL = 'N'
-    
-# create Mail object, login get 
-# number of unread emails, then logout
-g = Mail() 
-g.login()
-numUnread = g.getNumUnread()
-g.logout()
-
-# create SConnection object and connect
-# to serial port
-s = SConnection()
-s.connectToSerial('COM3')
-
-# write message to serial depending on if
-# there are unread emails or not
-if numUnread > 0:
-    s.writeToSerial(MAIL)
-else:
-    s.writeToSerial(NO_MAIL)
-
-# close serial connection
-s.closeSerial() 
-
-"""
